e0f992e667782c5dd3319e3a9c2923cd76d7bb20
Antonio Paniagua <44169302+HippedTony@users.noreply.github.com>
2026-02-04 17:18:11 -0600

    Nextjs first project app
    First complete Next.js 13 project

---
+++ nextjs-firstproject/app/about/page.jsx
@@ -0,0 +1,17 @@
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam magnam
+        quaerat cum aperiam molestiae excepturi optio debitis eum tenetur
+        numquam iste nemo consectetur dolores, a nulla, consequatur ut dolor
+        voluptatibus, voluptatem asperiores. Unde vitae recusandae dolorum odit
+        impedit reiciendis adipisci sint quibusdam qui amet, molestiae
+        doloremque doloribus neque mollitia similique!
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ nextjs-firstproject/app/services/page.jsx
@@ -0,0 +1,15 @@
+function ServicesPage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ea,
+        quas ipsam laboriosam placeat nesciunt earum, sapiente hic voluptates
+        autem exercitationem. Impedit nisi voluptas deserunt cupiditate maxime!
+        Quibusdam, at reiciendis?
+      </p>
+    </div>
+  );
+}
+
+export default ServicesPage;
