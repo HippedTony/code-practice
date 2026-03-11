5d5e880bcc31f150e3d93c77de2e9d94706ca920
Antonio Paniagua <44169302+HippedTony@users.noreply.github.com>
2026-03-10 22:11:40 -0600

    Fix: convert backend submodule to normal folder

---
+++ Nestjs-React-Mongodb/tasksapi/src/tasks/tasks.controller.ts
@@ -0,0 +1,58 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  NotFoundException,
+  Param, 
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from 'src/dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  findAll() {
+    return this.tasksService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const task = await this.tasksService.findOne(id);
+    if (!task) throw new NotFoundException('Task not found');
+    return task;
+  }
+
+  @Post()
+  create(@Body() body: CreateTaskDto) {
+    try {
+      return this.tasksService.create(body);
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Task already aexists');
+      }
+      throw error;
+    }
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async delete(@Param('id') id: string) {
+    const task = await this.tasksService.delete(id);
+    if (!task) throw new NotFoundException('Task not found');
+    return task;
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() body: any) {
+    const task = await this.tasksService.update(id, body);
+    if (!task) throw new NotFoundException('Task not found');
+    return task;
+  }
+}
