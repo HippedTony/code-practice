40bc1711433df16e52524e481172b0068307cfe1
Antonio Paniagua <44169302+HippedTony@users.noreply.github.com>
2026-03-17 14:26:46 -0600

    Project created

---
+++ next-auth-radix/app/auth/register/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@radix-ui/themes';
+import { BookmarkFilledIcon } from '@radix-ui/react-icons';
+
+function RegisterPage() {
+  return (
+    <div>
+      <Button>
+        <BookmarkFilledIcon /> Bookmark
+      </Button>
+    </div>
+  );
+}
+
+export default RegisterPage;
